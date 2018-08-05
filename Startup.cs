be75f377aec15792a283c918bcfcc31db5f72718
Sam Rae <samuelrae@gmail.com>
2018-08-06 00:10:44 +0100

    changed to sqlite db

--- Startup.cs
+++ Startup.cs
@@ -1,24 +1,31 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using BlogApi.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using BlogApi.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace BlogApi
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<PostContext>(opt => 
-                opt.UseInMemoryDatabase("PostList"));
-            services.AddMvc()
-                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseMvc();
-        }
+
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
     }
+    public IConfiguration Configuration { get; }
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<PostContext>(opt =>
+          opt.UseSqlite(Configuration.GetConnectionString("PostContext")));
+      services.AddMvc()
+              .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+    }
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseMvc();
+    }
+  }
 }
