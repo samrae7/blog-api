d6e0a4f20702f7b83dc36e3f9741efd563fe16a3
Sam Rae <samuelrae@gmail.com>
2018-08-19 16:50:02 +0100

    add S3UploadController

---
+++ Controllers/S3UploadController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using BlogApi.Models;
+using BlogApi.Services;
+using System.Web;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+using BlogApi;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class S3UploadController : ControllerBase
+  {
+    public string AWS_KEY {get; private set;}
+    public string AWS_SECRET {get; private set;}
+    public static IConfiguration Configuration { get; set; }
+    private AmazonUploader uploader { get; set; };
+
+    public S3UploadController(IConfiguration config)
+    {
+      Configuration = config;
+      AWS_KEY = Configuration["AWS_KEY"];
+      AWS_SECRET = Configuration["AWS_SECRET"];
+      uploader = new AmazonUploader(AWS_KEY, AWS_SECRET);
+    }
+
+    [HttpGet]
+    public ListObjectsResponse GetAll()
+    {
+      return uploader.ListingObjectsAsync().Result;
+    }
+      
+    [HttpPost]
+    public PutObjectResponse MyFileUpload()
+    {
+      var request = HttpContext.Request;
+      var fileStream = request.Body;
+      var contentLength = request.ContentLength;
+      var contentType = request.ContentType;
+      // string filePath = this.Request.Form.Files.First().FileName;
+
+      var length = contentLength.HasValue ? (long)contentLength : 0;
+      return uploader.sendMyFileToS3(fileStream, contentType, length).Result;
+    }
+  }
+}
