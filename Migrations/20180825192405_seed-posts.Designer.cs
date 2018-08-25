1f498c354818f877f7ed974934b383a65812b569
Sam Rae <samuelrae@gmail.com>
2018-08-25 21:46:59 +0100

    add seed data

--- Migrations/20180825192405_seed-posts.Designer.cs
+++ Migrations/20180825192405_seed-posts.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BlogApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogApi.Migrations
 {
     [DbContext(typeof(PostContext))]
-    partial class PostContextModelSnapshot : ModelSnapshot
+    [Migration("20180825192405_seed-posts")]
+    partial class seedposts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
